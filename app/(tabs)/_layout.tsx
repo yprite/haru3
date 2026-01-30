dcc41d557b27b6c9a4d5ca03b919183de0fc659e
yprite <yprite@gmail.com>
2026-01-30 22:02:06 +0900

    refactor: 프로필 탭 삭제

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -51,13 +51,6 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <TabBarIcon name="refresh" color={color} />,
         }}
       />
-      <Tabs.Screen
-        name="profile"
-        options={{
-          title: '프로필',
-          tabBarIcon: ({ color }) => <TabBarIcon name="person" color={color} />,
-        }}
-      />
     </Tabs>
   );
 }
